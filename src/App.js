077342177710eee421a2ae9a1a862c75218ed4b5
yuu <poscinre@gmail.com>
2022-10-19 00:24:11 +0900

    third

--- src/App.js
+++ src/App.js
@@ -12,22 +12,20 @@ function App() {
 
   return(
     <>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <AuthContextProvider>
-          <Navbar />
-          <Routes>
-            <Route path="/netflix" element={<Main />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-          </Routes>
-          <div className="empty"></div>
-          <Row rowID='1' title="인기 콘텐츠" fetchUrl={requests.fetchPopular} isLargeRow/>
-          <Row rowID='2' title="지금 뜨는 콘텐츠" fetchUrl={requests.fetchTrending} />
-          <Row rowID='3' title="공개 예정" fetchUrl={requests.fetchUpcoming} />
-          <Row rowID='4' title="호평받은 영화" fetchUrl={requests.fetchTopRated} />
-          <div className="empty"></div>
-        </AuthContextProvider>
-      </BrowserRouter>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/netflix" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+        <div className="empty"></div>
+        <Row rowID='1' title="인기 콘텐츠" fetchUrl={requests.fetchPopular} isLargeRow/>
+        <Row rowID='2' title="지금 뜨는 콘텐츠" fetchUrl={requests.fetchTrending} />
+        <Row rowID='3' title="공개 예정" fetchUrl={requests.fetchUpcoming} />
+        <Row rowID='4' title="호평받은 영화" fetchUrl={requests.fetchTopRated} />
+        <div className="empty"></div>
+      </AuthContextProvider>
     </>
   )
 }
